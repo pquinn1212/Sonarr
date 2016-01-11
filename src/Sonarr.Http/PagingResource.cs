b55c91ee62f8b7d662022fe8b18822784f6e410f
Keivan Beigi <me@keivan.io>
2016-01-11 00:22:23 -0800

    api v3

---
+++ src/Sonarr.Http/PagingResource.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using NzbDrone.Core.Datastore;
+
+namespace Sonarr.Http
+{
+    public class PagingResource<TResource>
+    {
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+        public string SortKey { get; set; }
+        public SortDirection SortDirection { get; set; }
+        public string FilterKey { get; set; }
+        public string FilterValue { get; set; }
+        public int TotalRecords { get; set; }
+        public List<TResource> Records { get; set; }
+    }
+
+    public static class PagingResourceMapper
+    {
+        public static PagingSpec<TModel> MapToPagingSpec<TResource, TModel>(this PagingResource<TResource> pagingSpec)
+        {
+            return new PagingSpec<TModel>
+            {
+                Page = pagingSpec.Page,
+                PageSize = pagingSpec.PageSize,
+                SortKey = pagingSpec.SortKey,
+                SortDirection = pagingSpec.SortDirection,
+            };
+        }
+    }
+}
